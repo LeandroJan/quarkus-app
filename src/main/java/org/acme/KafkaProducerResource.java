776ef5560ec6aab31069a51c34f3beb82f883f1a
LeandroJan <int84h@gmail.com>
2024-07-05 23:56:18 -0300

    disabling kafka

--- src/main/java/org/acme/KafkaProducerResource.java
+++ src/main/java/org/acme/KafkaProducerResource.java
@@ -1,23 +1,23 @@
-package org.acme;
+// package org.acme;
 
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
+// import org.eclipse.microprofile.reactive.messaging.Channel;
+// import org.eclipse.microprofile.reactive.messaging.Emitter;
 
-import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+// import jakarta.inject.Inject;
+// import jakarta.ws.rs.POST;
+// import jakarta.ws.rs.Path;
+// import jakarta.ws.rs.core.Response;
 
-@Path("/kafka")
-public class KafkaProducerResource {
+// @Path("/kafka")
+// public class KafkaProducerResource {
 
-    @Inject
-    @Channel("generated-data")
-    Emitter<String> emitter;
+//     @Inject
+//     @Channel("generated-data")
+//     Emitter<String> emitter;
 
-    @POST
-    public Response sendMessage(String message) {
-        emitter.send(message);
-        return Response.ok().build();
-    }
-}
+//     @POST
+//     public Response sendMessage(String message) {
+//         emitter.send(message);
+//         return Response.ok().build();
+//     }
+// }
