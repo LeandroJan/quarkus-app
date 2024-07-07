e1427f0f7c49dc54e924d40ccafec717c7c3bd73
LeandroJan <int84h@gmail.com>
2024-07-07 00:49:38 -0300

    add quarkus interface to sendo message

--- src/main/java/org/acme/KafkaProducerResource.java
+++ src/main/java/org/acme/KafkaProducerResource.java
@@ -1,23 +1,58 @@
 package org.acme;
 
+// import org.eclipse.microprofile.reactive.messaging.Channel;
+// import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+// import jakarta.inject.Inject;
+// import jakarta.ws.rs.POST;
+// import jakarta.ws.rs.Path;
+// import jakarta.ws.rs.core.Response;
+
+// @Path("/kafka")
+// public class KafkaProducerResource {
+
+//     @Inject
+//     @Channel("generated-data")
+//     Emitter<String> emitter;
+
+//     @POST
+//     public Response sendMessage(String message) {
+//         emitter.send(message);
+//         return Response.ok().build();
+//     }
+// }
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/kafka")
 public class KafkaProducerResource {
 
     @Inject
-    @Channel("generated-data")
+    @Channel("my-kafka-topic")
     Emitter<String> emitter;
 
+    @Inject
+    Template index;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        return index.instance();
+    }
+
     @POST
-    public Response sendMessage(String message) {
+    @Path("/send")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sendMessage(@FormParam("message") String message) {
         emitter.send(message);
-        return Response.ok().build();
+        return "Message sent: " + message;
     }
 }
