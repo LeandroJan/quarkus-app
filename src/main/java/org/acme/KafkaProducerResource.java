f8461465c4aa6f6032c3c7c01241ac427c05c39c
LeandroJan <int84h@gmail.com>
2024-07-07 02:38:48 -0300

    new html

--- src/main/java/org/acme/KafkaProducerResource.java
+++ src/main/java/org/acme/KafkaProducerResource.java
@@ -22,37 +22,39 @@ package org.acme;
 //     }
 // }
 
-import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/kafka")
 public class KafkaProducerResource {
 
-    @Inject
-    @Channel("my-kafka-topic")
+     @Inject
+    @Channel("my-topic")
     Emitter<String> emitter;
-
-    @Inject
-    Template index;
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get() {
-        return index.instance();
-    }
 
     @POST
     @Path("/send")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_PLAIN)
-    public String sendMessage(@FormParam("message") String message) {
-        emitter.send(message);
-        return "Message sent: " + message;
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response sendMessage(MessagePayload payload) {
+        emitter.send(payload.getMessage());
+        return Response.ok().entity("{\"status\":\"Message sent\"}").build();
+    }
+
+    public static class MessagePayload {
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
